e262f11f09645141458960286327325c1e2e01b6
EuanWorth <62507060+EuanWorth@users.noreply.github.com>
2024-12-02 17:48:52 +0000

    d

--- fynesse/address.py
+++ fynesse/address.py
@@ -27,13 +27,11 @@ def cross_validate(design_matrix, response_vector, k, n = 10, regularised = Fals
     alpha = 1.1 * np.sqrt(n) * sps.norm.ppf(1 - 0.05 / (2 * p))
   for _ in range(n):
     permutation = np.random.permutation(len(design_matrix))
-    design_matrix_folds = [design_matrix[permutation][i::k] for i in range(k)]
-    response_vector_folds = [response_vector[permutation][i::k] for i in range(k)]
+    design_matrix_folds = [design_matrix.iloc[permutation[i::k]] for i in range(k)]  
+    response_vector_folds = [response_vector.iloc[permutation[i::k]] for i in range(k)]  
     for i in range(k):
       train_design_matrix = np.concatenate(design_matrix_folds[:i] + design_matrix_folds[i+1:])
       train_response_vector = np.concatenate(response_vector_folds[:i] + response_vector_folds[i+1:])
-      test_design_matrix = design_matrix_folds[i]
-      test_response_vector = response_vector_folds[i]
       model = sm.OLS(train_response_vector, train_design_matrix*1)
       fit = 0
       if regularised:
